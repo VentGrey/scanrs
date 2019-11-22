8d65e17cf233356014d93c15712896d6ebd8e7a3
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-11-21 18:27:53 -0600

    Added basic functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
+use std::io;
+
+
+fn scand() -> i64 {
+    let mut int = String::new();
+    io::stdin().read_line(&mut int).expect("IO ERROR");
+
+    return int.parse::<i64>().expect("Input error");
+}
+
+fn scani() -> i64 {
+    let mut int = String::new();
+    io::stdin().read_line(&mut int).expect("IO ERROR");
+
+    return int.parse::<i64>().expect("Input error");
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
+    fn test_scand() {
         assert_eq!(2 + 2, 4);
     }
 }
