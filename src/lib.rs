1d1064a491634f98c99e161167906b9e012dcaa0
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-11-21 22:56:30 -0600

    Made all functions public

--- src/lib.rs
+++ src/lib.rs
@@ -9,21 +9,21 @@ pub fn scand() -> i64 {
     return int.parse::<i64>().expect("Input error");
 }
 
-fn scani() -> i64 {
+pub fn scani() -> i64 {
     let mut int = String::new();
     io::stdin().read_line(&mut int).expect("IO ERROR");
 
     return int.parse::<i64>().expect("Input error");
 }
 
-fn scand_32() -> i32 {
+pub fn scand_32() -> i32 {
     let mut int = String::new();
     io::stdin().read_line(&mut int).expect("IO ERROR");
 
     return int.parse::<i32>().expect("Input error");
 }
 
-fn scani_32() -> i32 {
+pub fn scani_32() -> i32 {
     let mut int = String::new();
     io::stdin().read_line(&mut int).expect("IO ERROR");
 
@@ -31,14 +31,14 @@ fn scani_32() -> i32 {
 }
 
 /* Scan Float Functions */
-fn scanfl() -> f64 {
+pub fn scanfl() -> f64 {
     let mut float = String::new();
     io::stdin().read_line(&mut float).expect("IO ERROR");
 
     return float.parse::<f64>().expect("Input Error");
 }
 
-fn scanfl_32() -> f32 {
+pub fn scanfl_32() -> f32 {
     let mut float = String::new();
     io::stdin().read_line(&mut float).expect("IO ERROR");
 
