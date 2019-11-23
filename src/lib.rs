b598b13dc595e1c259cb90337a5ad51b378cedfb
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-11-22 18:33:26 -0600

    Trim trailing whitespace

--- src/lib.rs
+++ src/lib.rs
@@ -6,28 +6,28 @@ pub fn scand() -> i64 {
     let mut int = String::new();
     io::stdin().read_line(&mut int).expect("IO ERROR");
 
-    return int.parse::<i64>().expect("Input error");
+    return int.trim().parse::<i64>().expect("Input error");
 }
 
 pub fn scani() -> i64 {
     let mut int = String::new();
     io::stdin().read_line(&mut int).expect("IO ERROR");
 
-    return int.parse::<i64>().expect("Input error");
+    return int.trim().parse::<i64>().expect("Input error");
 }
 
 pub fn scand_32() -> i32 {
     let mut int = String::new();
     io::stdin().read_line(&mut int).expect("IO ERROR");
 
-    return int.parse::<i32>().expect("Input error");
+    return int.trim().parse::<i32>().expect("Input error");
 }
 
 pub fn scani_32() -> i32 {
     let mut int = String::new();
     io::stdin().read_line(&mut int).expect("IO ERROR");
 
-    return int.parse::<i32>().expect("Input error");
+    return int.trim().parse::<i32>().expect("Input error");
 }
 
 /* Scan Float Functions */
@@ -35,13 +35,13 @@ pub fn scanfl() -> f64 {
     let mut float = String::new();
     io::stdin().read_line(&mut float).expect("IO ERROR");
 
-    return float.parse::<f64>().expect("Input Error");
+    return float.trim().parse::<f64>().expect("Input Error");
 }
 
 pub fn scanfl_32() -> f32 {
     let mut float = String::new();
     io::stdin().read_line(&mut float).expect("IO ERROR");
 
-    return float.parse::<f32>().expect("Input Error");
+    return float.trim().parse::<f32>().expect("Input Error");
 }
 
